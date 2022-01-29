e9192ea7ef1e768e4c7c1c55bc8f66a7f4d0dd25
Juan Calfucura <juan.calfucura@gmail.com>
2022-01-29 13:08:13 -0300

    modificacion LibreriasCompartidas4

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,32 +1,34 @@
-pipeline {
-    agent any
-    environment {
-        NEXUS_USER         = credentials('user-nexus')
-        NEXUS_PASSWORD     = credentials('password-nexus')
-    }
-    parameters {
-        choice(
-            name:'compileTool',
-            choices: ['Maven', 'Gradle'],
-            description: 'Seleccione herramienta de compilacion'
-        )
-    }
-    stages {
-        stage("Pipeline"){
-            steps {
-                script{
-                  switch(params.compileTool)
-                    {
-                        case 'Maven':
-                            //def ejecucion = load 'maven.groovy'
-                            //ejecucion.call()
-                            maven.call()
-                        break;
-                        case 'Gradle':
-                            //def ejecucion = load 'gradle.groovy'
-                            //ejecucion.call()
-                            gradle.call()
-                        break;
+def call() {
+    pipeline {
+        agent any
+        environment {
+            NEXUS_USER         = credentials('user-nexus')
+            NEXUS_PASSWORD     = credentials('password-nexus')
+        }
+        parameters {
+            choice(
+                name:'compileTool',
+                choices: ['Maven', 'Gradle'],
+                description: 'Seleccione herramienta de compilacion'
+            )
+        }
+        stages {
+            stage("Pipeline"){
+                steps {
+                    script{
+                    switch(params.compileTool)
+                        {
+                            case 'Maven':
+                                //def ejecucion = load 'maven.groovy'
+                                //ejecucion.call()
+                                maven.call()
+                            break;
+                            case 'Gradle':
+                                //def ejecucion = load 'gradle.groovy'
+                                //ejecucion.call()
+                                gradle.call()
+                            break;
+                        }
                     }
                 }
             }
